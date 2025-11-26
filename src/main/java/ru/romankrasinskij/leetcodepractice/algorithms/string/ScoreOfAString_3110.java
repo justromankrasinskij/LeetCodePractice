841b71fef33a0a5d96d4bb7838714620fe8d742d
Roman Krasinskij <justromankrasinskij@gmail.com>
2025-11-26 10:57:41 +0300

    Add solution to task #3110 Class: ScoreOfAString_3110

---
+++ src/main/java/ru/romankrasinskij/leetcodepractice/algorithms/string/ScoreOfAString_3110.java
@@ -0,0 +1,41 @@
+package ru.romankrasinskij.leetcodepractice.algorithms.string;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ScoreOfAString_3110 {
+
+    private static String s = "hello";
+    private static int charCount = s.length();
+
+    public static void main(String[] args) {
+        System.out.println("String: " + s + "\nScore of a string: " + getScoreOfAString(s));
+    }
+
+    private static List<Integer> getCharCodeList() {
+        List<Integer> charCodeList = new ArrayList<>(charCount);
+
+        for (int i = 0; i < charCount; i++) {
+            int charCode = s.charAt(i);
+            charCodeList.add(charCode);
+        }
+
+        return charCodeList;
+    }
+
+    private static List<Integer> getDifferentExpressionList() {
+        List<Integer> differentExpressionList = new ArrayList<>();
+
+        for (int i = 0, j = 0; i < (charCount - 1); i++) {
+            int differenceExpression = (int) Math.abs((getCharCodeList().get(j) - getCharCodeList().get(++j)));
+            differentExpressionList.add(differenceExpression);
+        }
+
+        return differentExpressionList;
+    }
+
+    private static int getScoreOfAString(String s) {
+        int sum = getDifferentExpressionList().stream().mapToInt(Integer::intValue).sum();
+        return sum;
+    }
+}
